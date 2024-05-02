8bbe5c35fa1ef4b6703401b033613d89b8907a8f
ArminRezaeii <arminrezaei53@gmail.com>
2024-05-02 07:31:19 +0330

    test

--- components/SignUpFrom.tsx
+++ components/SignUpFrom.tsx
@@ -18,7 +18,7 @@ function SignUpFrom() {
             .max(45, "Last name must be less than  45 characters")
             .regex(new RegExp("^[a-zA-z]+$"), "No special character allowed!"),
         email: z.string().email("Please enter a valid email address"),
-        // phone: z.string().refine(validator.isMobilePhone, "Please enter a vaild phone number!"),
+        phone: z.string().refine(validator.isMobilePhone, "Please enter a vaild phone number!"),
         password: z.string().min(6, "Password must be at least 6 characters").max(50, "Password must be less than 50 characters"),
         confrimPassword: z.string().min(6, "Password must be at least 6 characters").max(50, "Password must be less than 50 characters"),
         accepted: z.literal(true, {
@@ -41,12 +41,11 @@ function SignUpFrom() {
             <Input {...register("firstName")} label="First Name" startContent={<UserIcon className='w-4' />} />
             <Input {...register("lastName")} label="Last Name" startContent={<UserIcon className='w-4' />} />
             <Input {...register("email")} className='col-span-2' label="Email" startContent={<EnvelopeOpenIcon className='w-4' />} />
-            {/* <Input {...register("phone")} className='col-span-2' label="Phone" startContent={<PhoneIcon className='w-4' />} /> */}
+            <Input {...register("phone")} className='col-span-2' label="Phone" startContent={<PhoneIcon className='w-4' />} />
             <Input {...register("password")} className='col-span-2' label="Password" endContent={
                 isVisiblePass ? <EyeIcon className='w-4 cursor-pointer' onClick={toggleVisblePass} /> : <EyeSlashIcon className='w-4 cursor-pointer' onClick={toggleVisblePass} />
             } type={isVisiblePass ? "text" : "password"} startContent={<KeyIcon className='w-4' />} />
             <Input {...register("confrimPassword")} className='col-span-2' label="Confrim Password" type={isVisiblePass ? "text" : "password "} startContent={<KeyIcon className='w-4' />} />
-            
             <Checkbox {...register("accepted")}>Accpet the<Link href="/terms">Terms</Link></Checkbox>
             <Button color='primary' type='submit'>Submit</Button>
         </form>
