2fd175c38ca709f5545b42ca9dd9919b67fff637
ArminRezaeii <arminrezaei53@gmail.com>
2024-05-05 20:57:32 +0330

    zod form done

--- components/SignUpFrom.tsx
+++ components/SignUpFrom.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { EnvelopeOpenIcon, EyeIcon, EyeSlashIcon, KeyIcon, PhoneIcon, UserIcon } from '@heroicons/react/16/solid'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Checkbox, Input, Link } from '@nextui-org/react'
 import React, { useState } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import validator from 'validator'
 import { z } from 'zod'
 function SignUpFrom() {
@@ -28,25 +29,57 @@ function SignUpFrom() {
         })
     }).refine(data => data.password == data.confrimPassword, {
         message: "Password and confirm password doesn't match!",
-        path: ["password,confrimPassword"]
+        path: ["confrimPassword"]
     })
     type InputType = z.infer<typeof FormSchema>
-    const { register, handleSubmit, reset } = useForm<InputType>()
+    const { register, handleSubmit, reset, formState: { errors }, control } = useForm<InputType>({
+        resolver: zodResolver(FormSchema)
+    })
 
     const saveUser: SubmitHandler<InputType> = async (data) => {
         console.log(data)
     }
+
     return (
         <form onSubmit={handleSubmit(saveUser)} className='grid-cols-2 grid gap-3 p-2 ml-[304px] w-full shadow  border rounded-md'>
-            <Input {...register("firstName")} label="First Name" startContent={<UserIcon className='w-4' />} />
-            <Input {...register("lastName")} label="Last Name" startContent={<UserIcon className='w-4' />} />
-            <Input {...register("email")} className='col-span-2' label="Email" startContent={<EnvelopeOpenIcon className='w-4' />} />
-            <Input {...register("phone")} className='col-span-2' label="Phone" startContent={<PhoneIcon className='w-4' />} />
-            <Input {...register("password")} className='col-span-2' label="Password" endContent={
-                isVisiblePass ? <EyeIcon className='w-4 cursor-pointer' onClick={toggleVisblePass} /> : <EyeSlashIcon className='w-4 cursor-pointer' onClick={toggleVisblePass} />
-            } type={isVisiblePass ? "text" : "password"} startContent={<KeyIcon className='w-4' />} />
-            <Input {...register("confrimPassword")} className='col-span-2' label="Confrim Password" type={isVisiblePass ? "text" : "password "} startContent={<KeyIcon className='w-4' />} />
-            <Checkbox {...register("accepted")}>Accpet the<Link href="/terms">Terms</Link></Checkbox>
+            <Input
+                errorMessage={errors.firstName?.message}
+                isInvalid={!!errors.firstName}
+                {...register("firstName")} label="First Name" startContent={<UserIcon className='w-4' />} />
+            <Input
+                errorMessage={errors.lastName?.message}
+                isInvalid={!!errors.lastName}
+                {...register("lastName")} label="Last Name" startContent={<UserIcon className='w-4' />} />
+            <Input
+                errorMessage={errors.email?.message}
+                isInvalid={!!errors.email}
+
+                {...register("email")} className='col-span-2' label="Email" startContent={<EnvelopeOpenIcon className='w-4' />} />
+            <Input
+                errorMessage={errors.phone?.message}
+                isInvalid={!!errors.phone}
+                {...register("phone")} className='col-span-2' label="Phone" startContent={<PhoneIcon className='w-4' />} />
+            <Input
+                errorMessage={errors.password?.message}
+                isInvalid={!!errors.password}
+                {...register("password")} className='col-span-2' label="Password" endContent={
+                    isVisiblePass ? <EyeIcon className='w-4 cursor-pointer' onClick={toggleVisblePass} /> : <EyeSlashIcon className='w-4 cursor-pointer' onClick={toggleVisblePass} />
+                } type={isVisiblePass ? "text" : "password"} startContent={<KeyIcon className='w-4' />} />
+            <Input
+
+                errorMessage={errors.confrimPassword?.message}
+                isInvalid={!!errors.confrimPassword}
+                {...register("confrimPassword")} className='col-span-2' label="Confrim Password" type={isVisiblePass ? "text" : "password "} startContent={<KeyIcon className='w-4' />} />
+            <Controller
+                name='accepted'
+                control={control}
+                render={({field}) => (
+                    <Checkbox onChange={field.onChange} onBlur={field.onBlur} className='col-span-2'
+                        isInvalid={!!errors.confrimPassword}
+                    >Accpet the<Link href="/terms">Terms</Link></Checkbox>
+                )}
+            />
+            {!!errors.accepted&&<p className='text-red-500'>{errors.accepted.message}</p>}
             <Button color='primary' type='submit'>Submit</Button>
         </form>
     )
